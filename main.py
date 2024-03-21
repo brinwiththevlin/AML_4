fef70563ae2e53d807259527c5dbe98d2a4e20ce
Bryan Cora <corabj@mail.uc.edu>
2024-03-20 22:41:30 -0400

    fike update function

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 import numpy as np
+import random
 
 
 def main():
-    train_data = loadmat('characters.mat')
-    train_data = train_data['char3'][0]
-    test_data = loadmat('test.mat')
-    test_data = test_data['test'][0]
+    train_data = loadmat("characters.mat")
+    train_data = train_data["char3"][0]
+    test_data = loadmat("test.mat")
+    test_data = test_data["test"][0]
 
     # Flatten the data
-    patterns = [pattern.flatten() for pattern in train_data]
-    test_patterns = [pattern.flatten() for pattern in test_data]
+    patterns = [pattern.flatten().astype(int) for pattern in train_data]
+    # test_patterns = [pattern.flatten() for pattern in test_data]
     weights = sum([weight_matrix(pattern) for pattern in patterns])
 
-    # test "memory" recallk
-    recalled_patterns_and_energies = [updateNetwork(pattern, weights)
-                                      for pattern in test_patterns]
+    # test "memory" recall
+    recalled_patterns_and_energies = [
+        updateNetwork(pattern * 2 - 1, weights) for pattern in patterns
+    ]
 
     for i in range(len(patterns)):
-        transformationAnalysis(
-            i, patterns[i], recalled_patterns_and_energies[i])
+        transformationAnalysis(i, patterns[i], recalled_patterns_and_energies[i])
 
 
 def transformationAnalysis(i, pattern, recalled_pattern_and_energies):
@@ -28,29 +30,35 @@ def transformationAnalysis(i, pattern, recalled_pattern_and_energies):
     recalled_pattern, energies = recalled_pattern_and_energies
     recalled_pattern = recalled_pattern.reshape(12, 12)
 
-    fig, ax = plt.subplots(1, 2)
-    fig.suptitle(f'Pattern {i+1}')
-    ax[0].imshow(pattern, cmap='gray')
-    ax[0].set_title('Original Pattern')
-    ax[1].imshow(recalled_pattern, cmap='gray')
-    ax[1].set_title('Recalled Pattern')
+    fig, ax = plt.subplots(1, 3)
+    fig.suptitle(f"Pattern {i+1}")
+    ax[0].imshow(pattern, cmap="gray")
+    ax[0].set_title("Original Pattern")
+    ax[1].imshow(recalled_pattern, cmap="gray")
+    ax[1].set_title("Recalled Pattern")
+    ax[2].plot(energies)
+    ax[2].set_title("Energy")
+    ax[2].set_xlabel("Iterations")
+    ax[2].set_ylabel("Energy")
+    ax[2].yaxis.set_major_formatter(
+        FuncFormatter(lambda x, _: f"{int(x/1000)}k" if abs(x) > 1000 else x)
+    )
+    plt.subplots_adjust(
+        left=None, bottom=None, right=None, top=None, wspace=0.8, hspace=None
+    )
+
     plt.show()
 
-    plt.plot(energies)
-    plt.title(f'Energy of Pattern {i+1}')
-    plt.xlabel('Iterations')
-    plt.ylabel('Energy')
-    plt.show()
 
+def updateNetwork(pattern, weights, max_iterations=1000):
 
-def updateNetwork(pattern, weights, max_iterations=100):
     energies = [Energy(pattern, weights)]
     new_pattern = pattern.copy()
 
     for _ in range(max_iterations):
-        for i in np.random.permutation(len(new_pattern)):
-            weighted_sum = np.dot(weights[i], new_pattern)
-            new_pattern[i] = 1 if weighted_sum > 0 else 0
+        i = random.randint(0, len(pattern) - 1)
+        weighted_sum = np.dot(weights[i], new_pattern)
+        new_pattern[i] = 1 if weighted_sum >= 0 else -1
 
         current_energy = Energy(new_pattern, weights)
         energies.append(current_energy)
@@ -60,11 +68,16 @@ def updateNetwork(pattern, weights, max_iterations=100):
 
         pattern = new_pattern.copy()
 
-    return new_pattern, energies
+    return (new_pattern + 1) / 2, energies
 
 
 def Energy(pattern, weights):
-    return -0.5 * np.matmul(pattern.T, np.matmul(weights, pattern))
+    energy = 0
+    for i in range(len(pattern)):
+        for j in range(len(pattern)):
+            energy += weights[i][j] * pattern[i] * pattern[j]
+
+    return energy / (-2)
 
 
 def weight_matrix(query):
@@ -73,10 +86,10 @@ def weight_matrix(query):
     for i in range(len(query)):
         for j in range(len(query)):
             if i != j:
-                w_matrix[i][j] = (2*query[i] - 1) * (2*query[j] - 1)
+                w_matrix[i][j] = (2 * query[i] - 1) * (2 * query[j] - 1)
 
     return w_matrix
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
